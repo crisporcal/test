4b9a5e076f1bbea2723b0dabc990974fea4def9e
cpereira <cpereira@excelsis.com.py>
2022-09-06 11:39:41 -0400

    cambios test2

--- example.py
+++ example.py
@@ -1,20 +1,5 @@
 def git_operation():
  print("I am adding example.py file to the remote repository. Editado")
-#comentario
-#asd
-#asdddasdd
-
-
-#adsadsa
-
-#asdadddsa
-
 
 
 git_operation()
-
-
-
-
-
-#asd
